f2bff8be073c6e9d03a06751f9c18ed9aa78cd99
putan <hiranono@gmail.com>
2017-05-04 04:14:10 +0900

    [clean] add comment
    - and simplification

--- example/server.js
+++ example/server.js
@@ -1,6 +1,13 @@
+/**
+ * sample server
+ *
+ * start-up
+ *   $ node example/server.js
+ * access
+ *   $ curl http://localhost:8080/aaa/get/1 # {"result":"OK! aaa!"}
+ */
 const Stubon = require('../index.js').default;
 var stubon = new Stubon(`${__dirname}/stub`, {
     debug : true,
-    ssl   : true,
 });
-stubon.server().listen(8443);
+stubon.server().listen(8080);
