d1f186a0694f066be0bf8cc0337aa6d5dc4ae642
putan <hiranono@gmail.com>
2017-05-12 00:29:02 +0900

    [clean] Cleaning of the file

--- example/server.js
+++ example/server.js
@@ -4,9 +4,21 @@
  * start-up
  *   $ node example/server.js
  * access
- *   $ curl http://localhost:8080/aaa/get/1 # {"result":"OK! aaa!"}
+ *   $ curl http://localhost:8080/aaa/get // {"result":"OK! aaa!"}
  */
+
+// this is the example including.
+// if you installed by npm, and write with ES5,
+//
+//   var Stubon = required('stubon').default;
+//
+// if write with over ES2015,
+//
+//   import Stubon from 'stubon';
+//
 const Stubon = require('../index.js').default;
+
+// start server
 var stubon = new Stubon(`${__dirname}/stub`, {
     debug : true,
 });
