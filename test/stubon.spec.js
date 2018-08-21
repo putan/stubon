17f4e446df4eaa510f2e256e7a0a667194b0a286
putan <hiranono@gmail.com>
2018-08-22 00:32:46 +0900

    [clean] cleanup test.

--- test/stubon.spec.js
+++ test/stubon.spec.js
@@ -6,83 +6,95 @@ import sinon from 'sinon';
 
 describe('src/stubon.js Stubon', () => {
 
-    const stubon = new Stubon('./test/stub');
-    const app = stubon.server().listen(8081);
-    const hostname = 'http://localhost:8081';
-
-    it('can GET', (done) => {
-        fetch(`${hostname}/test/get/1`)
-            .then(res => res.json())
-            .then(json => {
-                expect(json.result).to.be.equal('OK!');
-                done();
-            });
-    });
-
-    it('can POST', (done) => {
-        fetch(`${hostname}/test/post/1`, {
-            method  : 'POST',
-        })
-            .then(res => res.json())
-            .then(json => {
-                expect(json.result).to.be.equal('OK! POST!');
-                done();
-            });
-    });
-
-    it('parameter match', (done) => {
-        fetch(`${hostname}/test/get/999`)
-            .then(res => res.json())
-            .then(json => {
-                expect(json.result).to.be.equal('OK! param!');
-                done();
-            });
-    });
-
-    it('query match', (done) => {
-        fetch(`${hostname}/test/get/1?hoge=a`)
-            .then(res => res.json())
-            .then(json => {
-                expect(json.result).to.be.equal('OK! query!');
-                done();
-            });
-    });
-
-    it('header match', (done) => {
-        fetch(`${hostname}/test/post/1`, {
-            method : 'POST',
-            headers : { 'x-method' : 'PUT' },
-        })
-            .then(res => res.json())
-            .then(json => {
-                expect(json.result).to.be.equal('OK! header!');
-                done();
-            });
-    });
-
-    it('wait', (done) => {
-        const start = new Date();
-        fetch(`${hostname}/test/get/1?wait=wait`)
-            .then(res => res.json())
-            .then(json => {
-                const end = new Date();
-                expect(json.result).to.be.equal('OK! wait!');
-                expect(end - start).to.be.above(1000);
-                done();
-            });
-    });
-
-    it('not found', (done) => {
-        fetch(`${hostname}/test/gets`)
-            .then(res => {
-                expect(res.status).to.be.equal(404)
-                return res.text();
+    describe('request', () => {
+        let app;
+        let hostname;
+        beforeEach(() => {
+            const stubon = new Stubon('./test/stub');
+            app = stubon.server().listen(8081);
+            hostname = 'http://localhost:8081';
+        });
+    
+        it('can GET', (done) => {
+            fetch(`${hostname}/test/get/1`)
+                .then(res => res.json())
+                .then(json => {
+                    expect(json.result).to.be.equal('OK!');
+                    app.close();
+                    done();
+                });
+        });
+    
+        it('can POST', (done) => {
+            fetch(`${hostname}/test/post/1`, {
+                method  : 'POST',
             })
-            .then(text => {
-                expect(text).to.be.equal('Not Found');
-                app.close();
-                done();
-            });
+                .then(res => res.json())
+                .then(json => {
+                    expect(json.result).to.be.equal('OK! POST!');
+                    app.close();
+                    done();
+                });
+        });
+    
+        it('parameter match', (done) => {
+            fetch(`${hostname}/test/get/999`)
+                .then(res => res.json())
+                .then(json => {
+                    expect(json.result).to.be.equal('OK! param!');
+                    app.close();
+                    done();
+                });
+        });
+    
+        it('query match', (done) => {
+            fetch(`${hostname}/test/get/1?hoge=a`)
+                .then(res => res.json())
+                .then(json => {
+                    expect(json.result).to.be.equal('OK! query!');
+                    app.close();
+                    done();
+                });
+        });
+    
+        it('header match', (done) => {
+            fetch(`${hostname}/test/post/1`, {
+                method : 'POST',
+                headers : { 'x-method' : 'PUT' },
+            })
+                .then(res => res.json())
+                .then(json => {
+                    expect(json.result).to.be.equal('OK! header!');
+                    app.close();
+                    done();
+                });
+        });
+    
+        it('wait', (done) => {
+            const start = new Date();
+            fetch(`${hostname}/test/get/1?wait=wait`)
+                .then(res => res.json())
+                .then(json => {
+                    const end = new Date();
+                    expect(json.result).to.be.equal('OK! wait!');
+                    expect(end - start).to.be.above(1000);
+                    app.close();
+                    done();
+                });
+        });
+    
+        it('not found', (done) => {
+            fetch(`${hostname}/test/gets`)
+                .then(res => {
+                    expect(res.status).to.be.equal(404)
+                    return res.text();
+                })
+                .then(text => {
+                    expect(text).to.be.equal('Not Found');
+                    app.close();
+                    done();
+                });
+        });
     });
 
     it('error', (done) => {
